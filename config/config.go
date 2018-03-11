994694c251f3d445ccdfa292cf58102fd04e9865
DennisDenuto <dennis.leon39@gmail.com>
2018-03-11 15:40:15 -0700

    Add config package to read saml config from json file

---
+++ config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import "encoding/json"
+import (
+	"gopkg.in/validator.v2"
+	"fmt"
+)
+
+type Config struct {
+	PrivateKey  string `json:"private_key" validate:"nonzero"`
+	Certificate string `json:"certificate" validate:"nonzero"`
+	Address     string `json:"address" validate:"nonzero"`
+}
+
+func NewConfig(configContent []byte) (*Config, error) {
+
+	config := &Config{}
+	err := json.Unmarshal(configContent, config)
+	if err != nil {
+		panic(err)
+	}
+	if err = validator.Validate(config); err !=  nil {
+		return nil, fmt.Errorf("invalid config %s", err)
+	}
+
+	return config, err
+}
---
+++ config/config_test.go
@@ -0,0 +1,72 @@
+package config_test
+
+import (
+	. "github.com/DennisDenuto/saml-idp/config"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/ginkgo/extensions/table"
+	"encoding/json"
+)
+
+var _ = Describe("Config", func() {
+	var config *Config
+	var err error
+	BeforeEach(func() {
+		config, err = NewConfig([]byte(`{
+					"address": "http://localhost",
+					"private_key": "abc",
+					"certificate": "def"
+				}`))
+	})
+
+	It("should generate config from json config file", func() {
+		Expect(err).NotTo(HaveOccurred())
+		Expect(config.PrivateKey).To(Equal("abc"))
+		Expect(config.Certificate).To(Equal("def"))
+		Expect(config.Certificate).To(Equal("def"))
+		Expect(config.Address).To(Equal("http://localhost"))
+	})
+
+	Context("when given an invalid json config file", func() {
+		var requiredFields map[string]string
+
+		BeforeEach(func() {
+			requiredFields = map[string]string{
+				"address":     "address",
+				"private_key": "key",
+				"certificate": "cert",
+			}
+		})
+
+		table.DescribeTable("invalid fields", func(invalidKey, invalidValue, errorDescription string) {
+			cfg := cloneMap(requiredFields)
+
+			cfg[invalidKey] = invalidValue
+
+			jsonBytes, err := json.Marshal(cfg)
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = NewConfig(jsonBytes)
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError(errorDescription))
+
+		},
+			table.Entry("invalid address", "address", "", "invalid config Address: zero value"),
+			table.Entry("invalid key", "private_key", "", "invalid config PrivateKey: zero value"),
+			table.Entry("invalid cert", "certificate", "", "invalid config Certificate: zero value"),
+		)
+
+	})
+
+})
+
+func cloneMap(requiredFields map[string]string) map[string]string {
+	configMap := map[string]string{}
+
+	for key, value := range requiredFields {
+		configMap[key] = value
+	}
+
+	return configMap
+}
